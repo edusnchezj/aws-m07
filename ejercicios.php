3bee23d18dc00762446b4aca77a34991f8729e79
edusnchezj <eduardsnchez@gmail.com>
2020-10-08 15:59:49 +0200

    Ejercicio 3

--- ejercicios.php
+++ ejercicios.php
@@ -3,6 +3,11 @@
 <head>
   <meta charset="utf-8">
   <title>PHP - Primeros pasos</title>
+  <?php
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
+  ?>
   <style>
   div {
     padding: 10px;
@@ -57,7 +62,40 @@
   <h3>Ejercicio 2</h3>
   <ul><li>Crea un calendario.</li></ul>
 
-  <a href="ejercicio2.php"><button>Ir al ejercicio 2</button></a>
+  <div id="ejercicio2">
+    <a href="ejercicio2.php">
+      <button>Ir al ejercicio 2</button>
+    </a>
+  </div>
   <!-- </ejercicio_2> -->
+
+  <!-- <ejercicio_3> -->
+  <h3>Ejercicio 3</h3>
+  <ul><li>A partir de un archivo de texto, coge los productos de una tienda y enseñalos.</li></ul>
+
+  <div id="ejercicio3">
+    <?php
+      $articles = file('ejercicio3txt.txt') or die("Error al obtener los productos.");
+      foreach ($articles as $article) {
+        $product[] = explode(';', $article);
+      }
+      echo '<table>
+        <tr style="text-transform: uppercase; font-weight: bold;">
+          <td>Producto</td>
+          <td>Descripción</td>
+          <td>Coste</td>
+        <tr>';
+      for ($c=0; $c < count($product); $c++) {
+        echo '
+        <tr>
+          <td>'.$product[$c][0].'</td>
+          <td>'.$product[$c][1].'</td>
+          <td>'.$product[$c][2].'€</td>
+        </tr>';
+      }
+      echo '</table>';
+    ?>
+  </div>
+  <!-- </ejercicio_3> -->
 </body>
 </html>
